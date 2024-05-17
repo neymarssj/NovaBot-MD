37b25e2f3f79408c59512a26914ef73206f8a95e
neymarssj <143804583+neymarssj@users.noreply.github.com>
2024-05-17 12:09:04 -0300

    Update buscadores.js

--- plugins/buscadores.js
+++ plugins/buscadores.js
@@ -15,10 +15,10 @@ const os = require('os')
 let user = global.db.data.users[m.sender]
 
 async function buscadores(m, command, conn, text, budy, from, fkontak, prefix, args, q, quoted, lang, lolkeysapi) {
-if (global.db.data.users[m.sender].registered < true) return m.reply(info.registra)
+if (global.db.data.user
 if (global.db.data.users[m.sender].banned) return
 if (command == 'yts' || command == 'ytsearch') {
-if (global.db.data.users[m.sender].level < 2) return m.reply(`${lenguaje['nivel']()} 2 ${lenguaje['nivel2']()} ${prefix}nivel`) 
+if (global.db.data.user
 if (!text) return m.reply(`${lenguaje.lengua.ejem}\n${prefix + command} anime`)
 const yts = require("youtube-yts");
 const search = await yts(text);
