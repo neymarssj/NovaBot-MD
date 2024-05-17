82cfaba7d9d8063c9bb62ca0281b4732328a43dc
neymarssj <143804583+neymarssj@users.noreply.github.com>
2024-05-17 12:16:10 -0300

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getRandom} = require('../libs/fuctions.js'); 
 
 async function enable(m, command, isGroupAdmins, text, command, args, isBotAdmins, isGroupAdmins, isCreator) {
-if (global.db.data.users[m.sender].registered < true) return m.reply(info.registra)
+
 let user = global.db.data.users[m.sender]
 if (command == 'enable' || command == 'configuracion' || command == 'configurar') {
 const {welcome, antilink, antiFake, antiArabe, detect, autosticker, antiNsfw, game2, modeadmin, chatbot, audios, autolevelup, antitoxic, antiprivado, anticall, antilink2, AntiTiktok, AntiTelegram, AntiFacebook, AntInstagram, AntiYoutube, AntiTwitter, autoread} = global.db.data.chats[m.chat];
